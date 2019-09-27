612ee90a3c53d141470c2cc335296596d9e35ce0
Andy Kwok <andykhkwok@gmail.com>
2019-09-26 21:30:56 -0700

    see file from web_server folder

--- web_server/main.py
+++ web_server/main.py
@@ -3,23 +3,37 @@ try:
 except:
     import socket
 
+
+
 response_404 = """HTTP/1.0 404 NOT FOUND
 
 <h1>404 Not Found</h1>
 """
 
+
+response_500 = """HTTP/1.0 500 INTERNAL SERVER ERROR
+
+<h1>500 Internal Server Error</h1>
+"""
+
+
 response_template = """HTTP/1.0 200 OK
 
 %s
 """
+
+
 import machine
 import ntptime, utime
 from machine import RTC
+from machine import Pin
+from time import sleep
+
+
 seconds = ntptime.time()
 rtc = RTC()
 rtc.datetime(utime.localtime(seconds))
 
-adc = machine.ADC(0)
 
 def time():
     body = """<html>
@@ -33,17 +47,47 @@ def time():
     return response_template % body
 
 
-def dummy():
+def dummy(pin, led, switch, adc):
     body = "This is a dummy endpoint"
 
+    return response_template % body
+
+def lighton(pin, led, switch, adc):
+    pin.value(1)
+    led.value(1)
+    body = "The light is ON."
+    return response_template % body
+
+def lightoff(pin, led, switch, adc):
+    pin.value(0)
+    led.value(0)
+    body = "The light is OFF."
+    return response_template % body
+
+def switch(pin, led, switch, adc):
+    body = '{' + 'state: {} '.format(switch.value()) + '}'
+    return response_template % body
+
+def photo(pin, led, switch, adc):
+    body = '{' + 'photo-senor value: {} '.format(adc.read()) + '}'
     return response_template % body
 
 handlers = {
     'time': time,
     'dummy': dummy,
+    'lighton': lighton,
+    'lightoff': lightoff,
+    'switch': switch,
+    'photo': photo,
 }
 
 def main():
+
+    pin = machine.Pin(9, machine.Pin.OUT)
+    led = machine.Pin(2, machine.Pin.OUT)
+    switch = machine.Pin(10, machine.Pin.IN) 
+    adc = machine.ADC(0)
+    
     s = socket.socket()
     ai = socket.getaddrinfo("0.0.0.0", 8080)
     addr = ai[0][-1]
@@ -69,11 +113,11 @@ def main():
             # Given the path, identify the correct handler to use
             path = req.decode().split("\r\n")[0].split(" ")[1]
             handler = handlers[path.strip('/').split('/')[0]]
-            response = handler()
+            response = handler(pin, led, switch, adc)
         except KeyError:
-            response = 404_response
+            response = response_404
         except Exception as e:
-            response = ""
+            response = response_500
             print(str(e))
 
         # A handler returns an entire response in the form of a multi-line string.
